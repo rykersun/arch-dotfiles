8251ed0351f5cfa2d1c38d3e1e3a4ca5e88a187d
Ryker Sun <rykersun2002@gmail.com>
2022-09-29 13:50:33 +0800

    update enable_dark_mode.sh

--- enable_dark_mode.sh
+++ enable_dark_mode.sh
@@ -1,1 +1,13 @@
-mkdir ~/.config/gtk-3.0 && touch ~/.config/gtk-3.0/settings.ini && echo "[Settings]" >> ~/.config/gtk-3.0/settings.ini && echo "gtk-application-prefer-dark-theme=1" >> ~/.config/gtk-3.0/settings.ini
+FILE=~/.config/gtk-3.0/settings.ini
+
+if [ -f "$FILE" ]; then
+    if grep -Fxq "gtk-application-prefer-dark-theme=1" $FILE; then
+        echo "ERROR! DARK MODE ALREADY ENABLED.";
+    else
+        echo "gtk-application-prefer-dark-theme=1" >> ~/.config/gtk-3.0/settings.ini
+        echo "DONE! DARK MODE IS NOW ENABLED."
+    fi
+else 
+    mkdir ~/.config/gtk-3.0 && touch ~/.config/gtk-3.0/settings.ini && echo "[Settings]" >> ~/.config/gtk-3.0/settings.ini && echo "gtk-application-prefer-dark-theme=1" >> ~/.config/gtk-3.0/settings.ini
+    echo "DONE! DARK MODE IS NOW ENABLED."
+fi
